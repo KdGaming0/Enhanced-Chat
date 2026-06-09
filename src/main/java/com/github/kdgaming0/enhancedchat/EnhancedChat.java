198db86ea1d31b08619777333e5e601e09667321
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2026-06-09 14:22:41 +0200

    init project

---
+++ src/main/java/com/github/kdgaming0/enhancedchat/EnhancedChat.java
@@ -0,0 +1,15 @@
+package com.github.kdgaming0.enhancedchat;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EnhancedChat implements ClientModInitializer {
+    public static final String MOD_ID = "enhanced_chat";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
